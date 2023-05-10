69ff93c12712a877d164a64792173e4111b3d4eb
maarxx <chris.lewark@gmail.com>
2023-05-10 00:01:26 -0400

    AssignedWorkbenchesComp is CompAssignableToPawn

--- AssignedWorkbenches/AssignedWorkbenchesComp.cs
+++ AssignedWorkbenches/AssignedWorkbenchesComp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,7 @@ using Verse;
 
 namespace AssignedWorkbenches
 {
-    public class AssignedWorkbenchesComp : ThingComp
+    public class AssignedWorkbenchesComp : CompAssignableToPawn
     {
         // This space intentionally left blank.
     }
