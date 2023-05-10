4d7c50c17e903c443f3139d5f78db43ca5100c63
maarxx <chris.lewark@gmail.com>
2023-05-09 23:44:33 -0400

    Hello World

---
+++ AssignedWorkbenches/Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace AssignedWorkbenches
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            Log.Message("Hello from Harmony in scope: com.github.harmony.rimworld.maarx.assignedworkbenches");
+            var harmony = new Harmony("com.github.harmony.rimworld.maarx.assignedworkbenches");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
