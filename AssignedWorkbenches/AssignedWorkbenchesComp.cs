1ab3792e625055178bd5fc32ff75c0ba942ac4fa
maarxx <chris.lewark@gmail.com>
2023-05-09 23:58:44 -0400

    Create AssignedWorkbenchesComp

---
+++ AssignedWorkbenches/AssignedWorkbenchesComp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AssignedWorkbenches
+{
+    public class AssignedWorkbenchesComp : ThingComp
+    {
+        // This space intentionally left blank.
+    }
+}
